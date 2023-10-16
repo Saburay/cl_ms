50e841ec9dc4e63d9dcf3519a5e425661ac717d6
saburay32 <saburay32@gmail.com>
2023-10-16 14:53:54 +0300

    some code

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Пользователь {username} был успешно создан!')
-            return redirect('blog-home')
+            messages.success(request, f'Аккаунт {username} был успешно создан!Введите имя пользователя и пароль.')
+            return redirect('user')
     else:
         form = UserOurRegistration()
     return render(request, 'users/registration.html', {'form': form, 'title': 'Регистрация пользователя'}, )
