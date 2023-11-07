eb633bf68df4df2140d38f1b11393df59f4a7295
saburay32 <saburay32@gmail.com>
2023-11-07 15:32:21 +0300

    added publicform

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import News
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
 
 def home(request):
     data = {
@@ -23,7 +23,21 @@ class ShowNewsView(ListView):
 
 class NewsDetailView(DetailView):
     model = News
+    template_name = 'blog/news_detail.html'
+    context_object_name = 'post'
 
+    def get_context_data(self, **kwards):
+        ctx = super(NewsDetailView, self).get_context_data(**kwards)
+        ctx['title'] = News.objects.filter(pk=self.kwargs['pk']).first()
+        return ctx
+
+class CreateNewsView(CreateView):
+    model = News
+    fields = ['title','text']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def contacts(request):
     return render(request, 'blog/contacts.html', {'title':'Contacts page'})
